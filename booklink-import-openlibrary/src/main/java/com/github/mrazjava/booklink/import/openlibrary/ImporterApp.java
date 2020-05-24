a0de914ee8ae1b075ea9c4e09b3b8138dc5033c8
Adam Zimowski <mrazjava@yandex.com>
2020-05-24 19:38:47 +0200

    refactoring: renamed data to import in import app

--- booklink-import-openlibrary/src/main/java/com/github/mrazjava/booklink/import/openlibrary/ImporterApp.java
+++ booklink-import-openlibrary/src/main/java/com/github/mrazjava/booklink/import/openlibrary/ImporterApp.java
@@ -1,6 +1,5 @@
 package com.github.mrazjava.booklink.data.openlibrary;
 
-import com.fasterxml.jackson.annotation.JacksonInject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
