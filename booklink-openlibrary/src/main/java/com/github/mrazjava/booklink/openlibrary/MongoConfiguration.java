95d8683d01c6f0ae33d9c6a77d8339d1e1067fce
Adam Zimowski <mrazjava@yandex.com>
2020-08-11 15:30:36 +0200

    ugprade spring boot parent
    this resolves mongo index startup warning

--- booklink-openlibrary/src/main/java/com/github/mrazjava/booklink/openlibrary/MongoConfiguration.java
+++ booklink-openlibrary/src/main/java/com/github/mrazjava/booklink/openlibrary/MongoConfiguration.java
@@ -1,8 +1,10 @@
 package com.github.mrazjava.booklink.openlibrary;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@Slf4j
 @Configuration
 @EnableMongoRepositories("com.github.mrazjava.booklink.openlibrary.repository")
 public class MongoConfiguration {
--- booklink-openlibrary/src/main/java/com/github/mrazjava/booklink/openlibrary/dataimport/ImporterApp.java
+++ booklink-openlibrary/src/main/java/com/github/mrazjava/booklink/openlibrary/dataimport/ImporterApp.java
@@ -8,6 +8,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 import java.io.File;
 
