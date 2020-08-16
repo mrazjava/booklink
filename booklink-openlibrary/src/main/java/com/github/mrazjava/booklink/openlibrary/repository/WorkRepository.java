db21cd6656059ed960a6a01ebd9d242b207f5843
Adam Zimowski <mrazjava@yandex.com>
2020-08-16 17:49:34 +0200

    refactoring: move openlibrary integration into a separate project

--- booklink-openlibrary/src/main/java/com/github/mrazjava/booklink/openlibrary/repository/WorkRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.mrazjava.booklink.openlibrary.repository;
-
-import com.github.mrazjava.booklink.openlibrary.schema.WorkSchema;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface WorkRepository extends MongoRepository<WorkSchema, String> {
-}
