fa5e3a31e0c05cc26189941e38ade5451e8dab41
Adam Zimowski <mrazjava@yandex.com>
2020-09-29 19:43:52 +0200

    cleanup

--- sandbox/persistence/mongo-init.sh
+++ sandbox/persistence/mongo-init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#mongorestore --username root --password pass123 --authenticationDatabase admin --nsInclude=openlibrary.* --verbose --gzip --archive=/tmp/mongodump/openlibrary-mongo.archive
 mongorestore --nsInclude=openlibrary.* --verbose --gzip --archive=/tmp/mongodump/openlibrary-mongo.archive
