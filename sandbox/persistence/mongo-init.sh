9eea9550934698e91bbd4b768cc87cc49ff15431
Adam Zimowski <mrazjava@yandex.com>
2020-09-29 19:46:23 +0200

    add comment

--- sandbox/persistence/mongo-init.sh
+++ sandbox/persistence/mongo-init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# openlibrary-mongo.archive is a binary dump of a database created with booklink-openlibrary import process
+# mongo binary archive is not provided (excluded with .gitignore) but the database itself is available via sandbox
+# docker image
+#
 mongorestore --nsInclude=openlibrary.* --verbose --gzip --archive=/tmp/mongodump/openlibrary-mongo.archive
